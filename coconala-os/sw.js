e542e55db1540f01f7756d53a1a604e1ed2dc682
shu-project-5296 <taikoshuhei@gmail.com>
2026-08-25 04:26:46 +0900

    Add Coconala OS service worker

---
+++ coconala-os/sw.js
@@ -0,0 +1,1 @@
+const CACHE='coconala-os-v1';const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
